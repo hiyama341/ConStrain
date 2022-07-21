d4213e6beb061db1f68422032b3b57532ddbc1aa
Lucas Levassor <lucaslevassor@gmail.com>
2022-07-21 10:40:46 +0200

    Classic rtds them

--- docs/conf.py
+++ docs/conf.py
@@ -30,8 +30,8 @@ import constrain
 # needs_sphinx = '1.0'
 
 # Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx.ext.autosummary']
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones. 
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx.ext.autosummary', 'sphinx.ext.napoleon']
 
 autosummary_generate = True 
 
@@ -49,7 +49,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'ConStrain'
-copyright = "2022, Lucas Levassor"
+copyright = "2022, Technical University of Denmark (DTU)"
 author = "Lucas Levassor"
 
 # The version info for the project you're documenting, acts as replacement
@@ -85,7 +85,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 # 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
