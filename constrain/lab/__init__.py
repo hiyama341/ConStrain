64d025bd7b256a881aa556a22f79ba37bc153e1c
Lucas Levassor <lucaslevassor@gmail.com>
2022-08-08 14:20:23 +0200

    updated init.py

--- constrain/lab/__init__.py
+++ constrain/lab/__init__.py
@@ -2,7 +2,3 @@
 
 __author__ = """Lucas Levassor"""
 __email__ = "luclev@biosustain.dtu.dk"
-
-from . import _version
-
-__version__ = _version.get_versions()["version"]
--- constrain/lims/__init__.py
+++ constrain/lims/__init__.py
@@ -3,6 +3,3 @@
 __author__ = """Lucas Levassor"""
 __email__ = "luclev@biosustain.dtu.dk"
 
-from . import _version
-
-__version__ = _version.get_versions()["version"]
