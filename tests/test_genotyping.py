80603635719f0a636acbb03bc4d61bbcfd04b14b
Lucas Levassor <lucaslevassor@gmail.com>
2022-08-08 14:10:21 +0200

    added boilerplate for testing_genotype.py

---
+++ tests/test_genotyping.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+# Test PCR module
+
+from pydna.dseqrecord import Dseqrecord
+from pydna.readers import read
+from pydna.amplify import pcr
+from pydna.primer import Primer
+from Bio.SeqRecord import SeqRecord
+
+# Importing the module we are  testing
+from constrain.test.genotyping import *
+
+
+
+def test_slicing_and_naming_seq_plates():
+    pass 
+
+
+def test_plat_seq_data_wrangler():
+    pass 
+
+
+def test_plate_AvgQual():
+    pass 
+
+
+def test_split_df_names():
+    pass 
+
+def test_concatenating_list_of_dfs():
+    pass 
+
+def test_pairwise_alignment_of_templates():
+    pass 
