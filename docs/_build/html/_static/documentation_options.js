81a4a39546fb87a6e683c1d2b9e1e282bfcc7a06
Lucas Levassor <lucaslevassor@gmail.com>
2022-09-26 17:06:40 +0200

    wip

--- docs/_build/html/_static/documentation_options.js
+++ docs/_build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.2.7+57.gfc7687b.dirty',
+    VERSION: '0.2.7+59.g40f6bc1',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
